cf9c1ffbd8b18a79573b23e8edfc96fe363cb5b0
Uday <udaykalyan@Udays-MacBook-Air.local>
2025-05-14 15:54:20 +0530

    modified URL for category images

--- src/category_images.js
+++ src/category_images.js
@@ -1,31 +1,154 @@
 export const imageUrls = [
-  "11b892879247017fdabde160f0785fef.svg",
-  "1c47c06ac2e293c09fa0fafa023c92de.svg",
-  "21714e4a1cddf969cd37a1708148a0b2.svg",
-  "227ab8bc89a8a895ad7002b329b855e9.svg",
-  "2d6f748b0bf40386ad95e501409fc392.svg",
-  "2fa9d290d7bbac4133f77b0de117488a.svg",
-  "355c71946db22cbd5439140ca9e3173f.svg",
-  "5835f8611630dea5bdf479bcf69a1a86.svg",
-  "5c77617e4a3717954690c1ae1c3d528b.svg",
-  "5fa2929b21b2838e3b50e5851824fc8f.svg",
-  "63625746483df02c05df565d30b01403.svg",
-  "666ef8fe4473e5bbfaee8bc77101711d.svg",
-  "6d8db75af0bffd2ff401b564d79929a3.svg",
-  "70645df9b5bc7694c8fa5c91e48e279b.svg",
-  "73c86f10c3dc58c3321a52ecc2f878bc.svg",
-  "7c8c15aaf400679204d70922addc3b4d.svg",
-  "8cdeb7b9f2b4208423c9a0e9ffaf9dae.svg",
-  "9d3d2b50e1886e3ad6adc0eb2dfc000f.svg",
-  "abe86046010a22af65a721cc7bc02544.svg",
-  "af90df6115d2e366ee51704bf4e6eff8.svg",
-  "c07127a6d57dafa0724a6afa6d45ae12.svg",
-  "c2ff0d48658167f6419ef15964d0cfc2.svg",
-  "c696a4876324e2e5b82d00f9e9456a12.svg",
-  "c6da0227f4110272ab73390cb95eeea9.svg",
-  "cb04cef1b57444938fa18d5bbcfe7261.svg",
-  "d16e6872dbc7db7a73102d0b38ab89a4.svg",
-  "d1afbe66aa0ee99fdbcb5d4418cb3b7d.svg",
-  "d55eebac38ffb4552057d6bbe985bc66.svg",
-  "inverter-icon.svg"
-];
+  {
+  id:1,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/cb04cef1b57444938fa18d5bbcfe7261.svg",
+  categoryName:"AC Repair & Services",
+  
+},
+{
+  id:2,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/227ab8bc89a8a895ad7002b329b855e9.svgs",
+  categoryName:"Carpenter Services",
+  
+},
+{
+  id:3,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/5835f8611630dea5bdf479bcf69a1a86.svg",
+  categoryName:"CCTV Repair & Services",
+},
+{
+  id:4,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/21714e4a1cddf969cd37a1708148a0b2.svg",
+  categoryName:"Chimney Repair & Services",
+},
+{
+  id:5,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/d55eebac38ffb4552057d6bbe985bc66.svg",
+  categoryName:"Computer/Laptop Repair & Services",
+},
+{
+  id:6,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/abe86046010a22af65a721cc7bc02544.svg",
+  categoryName:"Deep Cleaning Services",
+},
+{
+  id:7,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/8cdeb7b9f2b4208423c9a0e9ffaf9dae.svg",
+  categoryName:"Digital marketing",
+},
+{
+  id:8,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/1c47c06ac2e293c09fa0fafa023c92de.svg",
+  categoryName:"Dishwasher Repair & Services",
+},
+{
+ id:9,
+ categoryImage:"prnv-services-backend-development.up.railway.app/image/c07127a6d57dafa0724a6afa6d45ae12.svg",
+ categoryName:"Electrician Services",
+},
+{
+ id:10,
+ categoryImage:"prnv-services-backend-development.up.railway.app/image/d1afbe66aa0ee99fdbcb5d4418cb3b7d.svg",
+ categoryName:"Event Organizer Services",
+},
+{
+ id:11,
+ categoryImage:"prnv-services-backend-development.up.railway.app/image/666ef8fe4473e5bbfaee8bc77101711d.svg",
+ categoryName:"Gas Stove Repair/Services & Gas Pipe Installation Services",
+},
+{
+  id:12,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/d16e6872dbc7db7a73102d0b38ab89a4.svg",
+  categoryName:"Generator Repair & Services",
+},
+{
+  id:13,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/c6da0227f4110272ab73390cb95eeea9.svg",
+  categoryName:"Geyser Repair & Services",
+},
+{
+  id:14,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/5fa2929b21b2838e3b50e5851824fc8f.svg",
+  categoryName:"Health Insurance Services",
+},
+{
+  id:15,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/c696a4876324e2e5b82d00f9e9456a12.svg",
+  categoryName:"Interior Designers",
+},
+{
+  id:16,
+  categoryImage:"https://prnvservices.com//assets/inverter-icon.svg",
+  categoryName:"Inverter Repair Services",
+},
+{
+  id:17,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/7c8c15aaf400679204d70922addc3b4d.svg",
+  categoryName:"Lift Repair & Services",
+},
+{
+  id:18,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/9d3d2b50e1886e3ad6adc0eb2dfc000f.svg",
+  categoryName:"Microwave Repair & Services",
+},
+{
+  id:19,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/2d6f748b0bf40386ad95e501409fc392.svg",
+  categoryName:"Painting Services",
+},
+{
+  id:20,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/6d8db75af0bffd2ff401b564d79929a3.svg",
+  categoryName:"Pest Control Services",
+},
+{
+  id:21,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/355c71946db22cbd5439140ca9e3173f.svg",
+  categoryName:"Photography & Videography Services",
+},
+{
+  id:22,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/11b892879247017fdabde160f0785fef.svg",
+  categoryName:"Plumber Services",
+},
+{
+  id:23,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/56312c08c1bed381adb970def32b5f1a.jpg",
+  categoryName:"Real Estate",
+},
+{
+  id:24,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/5c77617e4a3717954690c1ae1c3d528b.svg",
+  categoryName:"Refrigerator Repair & Services",
+},
+{
+  id:25,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/63625746483df02c05df565d30b01403.svg",
+  categoryName:"Sanitization Services",
+},
+{
+  id:26,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/af90df6115d2e366ee51704bf4e6eff8.svg",
+  categoryName:"TV Repair & Services",
+},
+{
+  id:27,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/c2ff0d48658167f6419ef15964d0cfc2.svg",
+  categoryName:"Washing Machine Repair & Services",
+},
+{
+  id:28,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/73c86f10c3dc58c3321a52ecc2f878bc.svg",
+  categoryName:"Water Purifier Repair & Services",
+},
+{
+  id:29,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/2fa9d290d7bbac4133f77b0de117488a.svg",
+  categoryName:"Web Designing Services",
+},
+{
+  id:30,
+  categoryImage:"prnv-services-backend-development.up.railway.app/image/70645df9b5bc7694c8fa5c91e48e279b.svg",
+  categoryName:"Wood Polishing Services",
+},
+]
